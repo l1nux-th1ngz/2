8ac9a7ce23465bca32e390fcd6451af8f6c1d8b1
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2024-12-09 08:20:31 -0700

    Update update-source.sh

--- update-source.sh
+++ update-source.sh
@@ -4,7 +4,7 @@
 sudo apt-get -y install fasttrack-archive-keyring
 
 # Update package lists
- sudo apt-get update
+sudo apt-get update
 
 # Update sources.list with the new repositories to make them available systemwide
 sudo bash -c 'cat <<EOF > /etc/apt/sources.list
@@ -22,25 +22,10 @@ sudo apt-get update
 # Upgrade all packages
 sudo apt-get upgrade -y
 
-# Prompt for username to switch to
-read -p "Enter the username to switch to: " username
-
-# Try switching user with su, and if it fails, use sudo su
-if su - $username -c 'exit' &>/dev/null; then
-    su - $username -c '
-    git clone https://github.com/l1nux-th1ngz/bspwm1.git
-    cd bspwm1
-    chmod +x run_all.sh
-    ./run_all.sh
-    '
-elif sudo su - $username -c 'exit' &>/dev/null; then
-    sudo su - $username -c '
-    git clone https://github.com/l1nux-th1ngz/bspwm1.git
-    cd bspwm1
-    chmod +x run_all.sh
-    ./run_all.sh
-    '
+# Start update-kernel.sh if it exists
+if [ -f "update-kernel.sh" ]; then
+    chmod +x update-kernel.sh
+    ./update-kernel.sh
 else
-    echo "Failed to switch user to $username. Please check the username and try again."
-    exit 1
+    echo "update-kernel.sh not found."
 fi
